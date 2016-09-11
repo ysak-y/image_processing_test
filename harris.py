fadcd4e84b2e328ee35ceeec05f3c4d8f3e0f275
ysak-y <yoshiaki.0614@gmail.com>
2016-09-11 23:28:40 +0900

    second commit

--- harris.py
+++ harris.py
@@ -4,5 +4,6 @@ from skimage.viewer import ImageViewer
 camera = data.camera()
 camera_gray = color.rgb2gray(camera)
 harris = feature.corner_harris(camera_gray)
-viewer = ImageViewer(harris)
-viewer.show()
+#viewer = ImageViewer(harris)
+#viewer.show()
+print(feature.corner_peaks(harris))
